26f6c56408ba3fae3fd25e3cbc212cf64b9b41a9
Mateus Bonacina <89853026+MateusBona28@users.noreply.github.com>
2022-07-06 11:03:25 -0300

    fix: readicionando o globalProvider no app

--- capstone-m3/src/App.js
+++ capstone-m3/src/App.js
@@ -1,10 +1,13 @@
 import Routes from "./services/routes";
 import "./App.css";
+import { GlobalProvider } from "./providers/global"
 
 function App() {
   return (
     <div className="App">
-      <Routes />
+      <GlobalProvider>
+        <Routes />
+      </GlobalProvider>
     </div>
 
   );
